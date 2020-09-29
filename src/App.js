c68a7dbd93c7b383e8701e00af65908ddd8ee769
Fran <fmjerez@icloud.com>
2020-09-29 11:33:21 +0200

    UseContext done

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
-import React, { Fragment } from "react";
+import React from "react";
 import Header from "./components/Header";
 import Form from "./components/Form";
+import ProviderCategories from "./context/ContextCategory";
 
 function App() {
   //https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=Gin&c=Ordinary_Drink
   //https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${idReceta}
   return (
-    <Fragment>
+    <ProviderCategories>
       <Header />
       <div className="container mt-5">
         <div className="row">
           <Form />
         </div>
       </div>
-    </Fragment>
+    </ProviderCategories>
   );
 }
 
